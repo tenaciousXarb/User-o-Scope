304ad347b6f2a392b9ff5d15d1929afe6215c721
Ahmad Arabi <ahmadarabi2562@gmail.com>
2023-02-10 13:08:13 +0600

    Mapping profiles directory error fixed

---
+++ UserApp/UserApp/MappingProfiles/UserMappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using DAL.EF;
+
+namespace AppUser.MappingProfiles
+{
+    public class UserMappingProfiles : Profile
+    {
+        public UserMappingProfiles()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, UserCreationDTO>().ReverseMap();
+        }
+    }
+}
